06267f6827273a98e474b096053b828d371a0ff7
NikiRyz <37452634+NikiRyz@users.noreply.github.com>
2019-11-24 11:18:00 +0300

    removed the store from the global scope

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,7 +3,7 @@ import { rootReducer } from '../reducers'
 import thunkMiddleware from 'redux-thunk'
 
  let store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
- 
- window.store = store;
- 
+  
  export default store;
+
+
